6c2ec16a109c7588d1f523323bf1e8799195b851
Amarnath Tripathy <114403813+amarrr11@users.noreply.github.com>
2023-02-22 12:21:45 +0530

    Add files via upload

---
+++ Print  if digit or not.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    int a;
+    printf("enter a");
+    scanf("%d",&a);
+    if(type(a)=="%d")
+        {printf("a is digit");}
+
+    else{
+        printf("not a digit");}
+}
---
+++ average of three integers.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    float x,y,z;
+    float avg;
+    printf("enter x");
+    scanf("%f",&x);
+    printf("enter y");
+    scanf("%f",&y);
+    printf("enter z");
+    scanf("%f",&z);
+    avg=((x+y+z)/3);
+    printf("%f",avg);
+}
